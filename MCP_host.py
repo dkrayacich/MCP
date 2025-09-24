8129b1973b4727c81b47bf3ace53fa0a0fd94fdd
dkrayacich <david.krayacich@gmail.com>
2025-09-23 21:42:42 -0400

    added to host

--- MCP_host.py
+++ MCP_host.py
@@ -4,3 +4,8 @@ import ollama
 from mcp.client.stdio import StdioServer
 
 MODEL = "llama3.1"
+
+server = StdioServer(command=[sys.executable, "server.py"])
+client = server.start()
+
+tools = client.list_tools().result()
